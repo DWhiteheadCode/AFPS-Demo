eb5292e980cd77fa39ec2ff81fd59f1e70f82962
DWhiteheadCode <95430831+DWhiteheadCode@users.noreply.github.com>
2024-09-21 15:03:00 +0800

    Basic input

--- Source/AFPS_Demo/Public/APlayerCharacter.h
+++ Source/AFPS_Demo/Public/APlayerCharacter.h
@@ -2,9 +2,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+#include "InputActionValue.h"
+
 #include "APlayerCharacter.generated.h"
 
 class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class AFPS_DEMO_API AAPlayerCharacter : public ACharacter
@@ -21,5 +26,26 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	TObjectPtr<UCameraComponent> CameraComp;
+
+	// INPUT ------------------------------------------------------------
+	// IMC
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultInputMappingContext;
+
+	// Move
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> MovementAction;
+
+	void Move(const FInputActionValue& Value);
+
+	// Look
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	void Look(const FInputActionValue& Value);
+
+	// Jump
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
 
 };
